b8bd82494ced88e15b23f1f0fb6657592b896211
Thorsteinnag <42977744+Thorsteinnag@users.noreply.github.com>
2020-05-07 23:24:42 +0000

    Setti inn virkni fyrir sjá nánar síðu og virkni til að bæta við vörum

---
+++ product/forms/product_form.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from product.models import Product
+
+class ProductCreateForm(ModelForm):
+    image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = Product
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'description': widgets.TextInput(attrs={'class': 'form-control'}),
+            'category': widgets.Select(attrs={'class': 'form-control'}),
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'manufacturer': widgets.Select(attrs={'class': 'form-control'}),
+            'on_sale': widgets.CheckboxInput(attrs={'class': 'checkbox'})
+        }
--- product/models.py
+++ product/models.py
@@ -4,6 +4,8 @@ from manufacturer.models import Manufacturer
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -12,7 +14,11 @@ class Product(models.Model):
     on_sale = models.BooleanField()
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class ProductImage(models.Model):
     image = models.CharField(max_length=999)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.image
