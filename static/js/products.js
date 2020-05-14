460f214a8762ec8f57a2aebe2b7380e53ae163da
Thorsteinnag <42977744+Thorsteinnag@users.noreply.github.com>
2020-05-14 21:01:36 +0000

    sortið virkar

--- static/js/products.js
+++ static/js/products.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                                 <h5 class="product-card-name">${d.name}</h5>
                                 <div class="card-body">
                                     <p class="text-truncate align-items-center">${d.description}</p>
-                                    <p${d.price}</p>
+                                    <p>${d.price} $</p>
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div class="product-button-div product-info-button">
                                             <a href="/product/${d.id}" type="button" 
@@ -53,7 +53,6 @@ $(document).ready(function () {
             type: 'GET',
             success: function (resp) {
                 var newHtml = resp.data.map(d => {
-
                     return `<section class="product_section" id="product">
                                 <div class="card single-product">
                                     <img src="${d.firstImage}" 
@@ -65,7 +64,7 @@ $(document).ready(function () {
                                 <h5 class="product-card-name">${d.name}</h5>
                                 <div class="card-body">
                                     <p class="text-truncate align-items-center">${d.description}</p>
-                                    <p${d.price}</p>
+                                    <p>${d.price} $</p>
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div class="product-button-div product-info-button">
                                             <a href="/product/${d.id}" type="button" 
@@ -88,6 +87,52 @@ $(document).ready(function () {
             }
         })
     })
-
 })
 
+$(document).ready(function () {
+    $('input[type="button"]').click(function () {
+        var the_order = $(this).val()
+        $.ajax({
+            url:'/product/?order_by=' + the_order,
+            type: 'GET',
+            success: function (resp) {
+                var newHtml = resp.data.map(d => {
+                    return `<div class="row row-cols-3">
+                                <div class="col-md-4">
+                                <section class="product_section" id="product">
+                                <div class="card single-product">
+                                    <img src="${d.firstImage}" 
+                                    class="bd-placeholder-img card-img-top product-card-img" 
+                                    width="225" height="225" 
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    preserveAspectRatio="xMidYMid slice" focusable="false" 
+                                    role="img" aria-label="Placeholder: Thumbnail" />
+                                <h5 class="product-card-name">${d.name}</h5>
+                                <div class="card-body">
+                                    <p class="text-truncate align-items-center">${d.description}</p>
+                                    <p>${d.price} $</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="product-button-div product-info-button">
+                                            <a href="/product/${d.id}" type="button" 
+                                                class="btn btn-sm btn-outline-secondary
+                                                product-button">Info</a>
+                                        </div>
+                                        <div class="product-button-div product-buy-button">
+                                            <button type="button" class="btn btn-sm
+                                            btn-outline-secondary product-button">Buy</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                </section>
+                                </div>
+                            </div>`
+                });
+                $('#product_filter_stuff').html(newHtml.join(''));
+            },
+            error: function (xhr, status, error) {
+                console.error(error);
+            }
+        })
+    })
+})
